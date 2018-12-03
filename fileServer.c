11eac0363607317bc0e31da6b2abf68b691bf8ea
Dancikowski <Dancikowski@gmail.com>
2018-12-03 23:52:38 +0100

    Change names of variables

--- fileServer.c
+++ fileServer.c
@@ -7,11 +7,13 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <dirent.h>
+#include <unistd.h>
 
 
-#define PORT htons(4448)
 
-void sendFilesList(int gn){
+#define PORT htons(4444)
+
+void sendFilesList(int sckt){
      DIR *mydir;
     struct dirent *myfile;
     struct stat mystat;
@@ -38,47 +40,43 @@ void sendFilesList(int gn){
             size_t fileNameLenght = strlen(myfile->d_name);
             names[i] = malloc((fileNameLenght+1) * sizeof(char));
             strcpy(names[i], myfile->d_name);
-            printf("Name is %s\n", names[i]);
             i++;
         }
     }
     closedir(mydir);
 
-    send(gn, &counter, sizeof(int),0 );
+    send(sckt, &counter, sizeof(int),0 );
 
     
     for (int i=0; i<counter; i++){
             int len = strlen(names[i]);
-            printf("len: %d\n", len);
-            send(gn, &len, sizeof(int),0);
-            send(gn, names[i], strlen(names[i]),0 );
+            send(sckt, &len, sizeof(int),0);
+            send(sckt, names[i], strlen(names[i]),0 );
     }
 
 }
 
-void ObsluzPolaczenie(int gn)
+void ObsluzPolaczenie(int sckt)
 {
     char sciezka[512];
-    long long dl_pliku, wyslano, wyslano_razem, przeczytano;
+    long long file_len, sentBytes, totalSendBytes, readBytes;
     struct stat fileinfo;
-    FILE* plik;
+    FILE* file;
     unsigned char bufor[1024];
     unsigned char myFileSize[1024];
     
     memset(sciezka, 0, 512);
-    if (recv(gn, sciezka, 512, 0) <= 0)
+    if (recv(sckt, sciezka, 512, 0) <= 0)
     {
-        printf("Potomny: blad przy odczycie sciezki\n");
+        printf("Blad przy odczycie sciezki\n\n");
         return;
     }
     
-    /*char *sciezka = sciezka +1;*/
-
-    printf("Potomny: klient chce plik %s\n", sciezka);
+    printf("Klient chce pobrać plik o następującej ścieżce: %s\n", sciezka);
     
     if (stat(sciezka, &fileinfo) < 0)
     {
-        printf("Potomny: nie moge pobrac informacji o pliku\n");
+        printf("Nie mozna pobrac informacji o pliku\n");
         return;
     }
     
@@ -88,43 +86,56 @@ void ObsluzPolaczenie(int gn)
         return;
     }
     
-    printf("Potomny: dlugosc pliku: %d\n", fileinfo.st_size);
+    printf("Dlugosc pliku: %d B\n\n", fileinfo.st_size);
     
-    dl_pliku = (long) fileinfo.st_size;
+    file_len = (long) fileinfo.st_size;
 
-    if (send(gn, &dl_pliku, sizeof(long), 0) != sizeof(long))
+    if (send(sckt, &file_len, sizeof(long), 0) != sizeof(long))
     {
-        printf("Potomny: blad przy wysylaniu wielkosci pliku\n");
+        printf("Blad przy wysylaniu wielkosci pliku\n");
         return;
     }
     
-    dl_pliku = fileinfo.st_size;
-    wyslano_razem = 0;
-    plik = fopen(sciezka, "rb");
-    if (plik == NULL)
+    file_len = fileinfo.st_size;
+    totalSendBytes = 0;
+    file = fopen(sciezka, "rb");
+    if (file == NULL)
     {
-        printf("Potomny: blad przy otwarciu pliku\n");
+        printf("Blad przy otwarciu pliku\n");
         return;
     }
+
+    int divide = fileinfo.st_size / 100;
+    int i =1;
+
+    fprintf(stderr, "Wysyłanie pliku: [");
     
-    while (wyslano_razem < dl_pliku)
+    while (totalSendBytes < file_len)
     {
-        przeczytano = fread(bufor, 1, 1024, plik);
-        wyslano = send(gn, bufor, przeczytano, 0);
-        if (przeczytano != wyslano){
+        readBytes = fread(bufor, 1, 1024, file);
+        sentBytes = send(sckt, bufor, readBytes, 0);
+        if (readBytes != sentBytes){
             break;
         }
-        wyslano_razem += wyslano;
-        /*printf("Potomny: wyslano %d bajtow\n", wyslano_razem);*/
-    }
-    
-    if (wyslano_razem == dl_pliku){
+        totalSendBytes += sentBytes;
+        if (totalSendBytes >= divide * i){
 
-        printf("Potomny: plik wyslany poprawnie\n");
+            for (int k=0; k < totalSendBytes / (divide * i); k++){
+                fprintf(stderr, "#");   
+            }
+                i++;
+            }
+        }
+    
+    if (totalSendBytes == file_len){
+
+        fprintf(stderr, "]");
+        printf("\n");
+        printf("Plik wyslany poprawnie!\n\n");
     }
     else
-        printf("Potomny: blad przy wysylaniu pliku\n");
-    fclose(plik);
+        printf("Blad przy wysylaniu pliku\n");
+    fclose(file);
 
    
     return;    
@@ -133,83 +144,73 @@ void ObsluzPolaczenie(int gn)
 
 int main(void)
 {
-    int gn_nasluch, gn_klienta;
+    int sckt_listen, sckt_client;
     struct sockaddr_in adr;
-    socklen_t dladr = sizeof(struct sockaddr_in);
+    socklen_t adr_len = sizeof(struct sockaddr_in);
     char command[10];
-    gn_nasluch = socket(PF_INET, SOCK_STREAM, 0);
+    sckt_listen = socket(PF_INET, SOCK_STREAM, 0);
     adr.sin_family = AF_INET;
     adr.sin_port = PORT;
     adr.sin_addr.s_addr = INADDR_ANY;
     memset(adr.sin_zero, 0, sizeof(adr.sin_zero));
     
-    if (bind(gn_nasluch, (struct sockaddr*) &adr, dladr) < 0)
+    if (bind(sckt_listen, (struct sockaddr*) &adr, adr_len) < 0)
     {
-        printf("Glowny: bind nie powiodl sie\n");
+        printf("Bind nie powiodl sie\n");
         return 1;
     }
     
-    listen(gn_nasluch, 10);
+    listen(sckt_listen, 10);
     
     while(1)
     {
-        dladr = sizeof(struct sockaddr_in);
-        gn_klienta = accept(gn_nasluch, (struct sockaddr*) &adr, &dladr);
-        if (gn_klienta < 0)
+        adr_len = sizeof(struct sockaddr_in);
+        sckt_client = accept(sckt_listen, (struct sockaddr*) &adr, &adr_len);
+        if (sckt_client < 0)
         {
-            printf("Glowny: accept zwrocil blad\n");
+            printf("Accept zwrocil blad\n");
             continue;
         }
-        printf("Glowny: polaczenie od %s:%u\n", 
+        printf("Polaczenie od %s:%u\n\n", 
             inet_ntoa(adr.sin_addr),
             ntohs(adr.sin_port)
             );
-        printf("Glowny: tworze proces potomny\n");
         if (fork() == 0)
         {
             while(1){
-            printf("Potomny: zaczynam obsluge\n");
-            
             memset(command, 0, 10);
-            if (recv(gn_klienta, command, 10, 0) <= 0)
+            if (recv(sckt_client, command, 10, 0) <= 0)
             {
-            printf("Potomny: blad przy odczycie sciezki\n");
+            printf("\nBlad przy odczycie sciezki\n");
             exit(1);
             }
 
             else {
                 if(strcmp(command, ":exit") == 0){
 
-                    printf("Zamykam\n");
-                    close(gn_klienta);
+                    printf("Zamykam polaczenie\n");
+                    close(sckt_client);
                     exit(1);
                 }
                 if(strcmp(command, ":file") == 0){
-                    printf("Wysyłam listę\n");
-                    sendFilesList(gn_klienta);
+                    sendFilesList(sckt_client);
                    
                     
                 }
                 if(strcmp(command, ":download") == 0){
-                    printf("Wysyłam plik\n");
-                    ObsluzPolaczenie(gn_klienta);
+                    ObsluzPolaczenie(sckt_client);
 
                 }
 
-                else{
-                    printf("%s\n", command);
-                }
 
             }
-            printf("Potomny: zamykam gniazdo\n");
-            printf("Potomny: koncze proces\n");
+            
 
             }
         }
         else
         {
             /* proces macierzysty */
-            printf("Glowny: wracam do nasluchu\n");
             continue;
         }
     }
